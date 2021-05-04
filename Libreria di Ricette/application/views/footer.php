e0e7b3d6344d943a2049f8281eda94a924e08d97
Mohammad Daffa Haris <46914307+Mohammaddaffaharis@users.noreply.github.com>
2021-05-04 10:39:00 +0700

    Update footer.php

--- Libreria di Ricette/application/views/footer.php
+++ Libreria di Ricette/application/views/footer.php
@@ -119,33 +119,34 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
     <!--/ footer  -->
 
     <!-- JS here -->
-    <script src="js/vendor/modernizr-3.5.0.min.js"></script>
-    <script src="js/vendor/jquery-1.12.4.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/owl.carousel.min.js"></script>
-    <script src="js/isotope.pkgd.min.js"></script>
-    <script src="js/ajax-form.js"></script>
-    <script src="js/waypoints.min.js"></script>
-    <script src="js/jquery.counterup.min.js"></script>
-    <script src="js/imagesloaded.pkgd.min.js"></script>
-    <script src="js/scrollIt.js"></script>
-    <script src="js/jquery.scrollUp.min.js"></script>
-    <script src="js/wow.min.js"></script>
-    <script src="js/nice-select.min.js"></script>
-    <script src="js/jquery.slicknav.min.js"></script>
-    <script src="js/jquery.magnific-popup.min.js"></script>
-    <script src="js/plugins.js"></script>
-    <script src="js/gijgo.min.js"></script>
+	//Menampilkan Javasript yang digunakan dalam website Libreria di Ricette
+    <script src="<?= base_url('assets/js/vendor/modernizr-3.5.0.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/vendor/jquery-1.12.4.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/popper.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/bootstrap.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/owl.carousel.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/isotope.pkgd.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/ajax-form.js') ?>"></script>
+    <script src="<?= base_url('assets/js/waypoints.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/jquery.counterup.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/imagesloaded.pkgd.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/scrollIt.js') ?>"></script>
+    <script src="<?= base_url('assets/js/jquery.scrollUp.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/wow.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/nice-select.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/jquery.slicknav.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/jquery.magnific-popup.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/plugins.js') ?>"></script>
+    <script src="<?= base_url('assets/js/gijgo.min.js') ?>"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
     <!--contact js-->
-    <script src="js/contact.js"></script>
-    <script src="js/jquery.ajaxchimp.min.js"></script>
-    <script src="js/jquery.form.js"></script>
-    <script src="js/jquery.validate.min.js"></script>
-    <script src="js/mail-script.js"></script>
+    <script src="<?= base_url('assets/js/contact.js') ?>"></script>
+    <script src="<?= base_url('assets/js/jquery.ajaxchimp.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/jquery.form.js') ?>"></script>
+    <script src="<?= base_url('assets/js/jquery.validate.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/mail-script.js') ?>"></script>
 
-    <script src="js/main.js"></script>
+    <script src="<?= base_url('js/main.js') ?>"></script>
 </body>
 
 </html>
