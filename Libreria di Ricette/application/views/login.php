55c2d88b7f34c7a8b9bd31e03bdb6dae0aeab74c
Otniel113 <57952404+Otniel113@users.noreply.github.com>
2021-05-04 10:32:52 +0700

    Update login.php

--- Libreria di Ricette/application/views/login.php
+++ Libreria di Ricette/application/views/login.php
@@ -5,15 +5,15 @@
     <link rel="stylesheet" href="<?= base_url('assets/css/stylesheet.css')?>">
   </head>
   <body>
-    <div class="container">
-        <div class="inputs">
+    <div class="container">		//container berupa kotak untuk menampung yang akan diinputkan
+        <div class="inputs">	//Isi dari container
             <form action="<?=site_url('AccountController/login')?>" method="POST">
                 <h2 style="text-align: center;">Login</h2>
                 <label><b>Username</b></label>
                     <input type="text" name="username" placeholder="Username" class="input-field"/>
                 <label><b>Password</b></label>
                 <input type="password" name="password" placeholder="Password" class="input-field"/>
-                <div class="btn">
+                <div class="btn">	//Tombol untuk login
                     <button>Login</button>
                 </div>
             </form>
