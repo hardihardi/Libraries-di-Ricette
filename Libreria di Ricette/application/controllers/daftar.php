b22725020eed6c61bacd5327cf6a09403972b9b4
AzureStream <46711225+AzureStream@users.noreply.github.com>
2021-05-04 11:19:58 +0700

    controller documentation
    add some comment explaining things on controller files

--- Libreria di Ricette/application/controllers/daftar.php
+++ Libreria di Ricette/application/controllers/daftar.php
@@ -3,21 +3,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class daftar extends CI_Controller {
 
+    // construct function that loads Account Model
 	function __construct()
 	{
 		parent::__construct();
 		$this->load->model('Account');
 	}
 
+    // function index that will take you to the register page
 	public function index()
 	{
 		$this->load->view('register');
 	}
 
+    // function to register as a member, the password will be hashed with md5
     public function register() {
         $dataUser = array(
             "username" => $this->input->post('username', true),
-            "password" => $this->input->post('password', true),
+            "password" => md5($this->input->post('password', true)),
             "isAdmin" => 0
         );
 
