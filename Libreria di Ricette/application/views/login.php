e3a517363e7236029d0776daf3869a50815f24ae
Mohammad Daffa Haris <46914307+Mohammaddaffaharis@users.noreply.github.com>
2021-05-04 10:37:04 +0700

    Create login.php

--- Libreria di Ricette/application/views/login.php
+++ Libreria di Ricette/application/views/login.php
@@ -5,15 +5,16 @@
     <link rel="stylesheet" href="<?= base_url('assets/css/stylesheet.css')?>">
   </head>
   <body>
-    <div class="container">		//container berupa kotak untuk menampung yang akan diinputkan
-        <div class="inputs">	//Isi dari container
+    <div class="container">
+        <div class="inputs">
+					//form yang digunakan untuk melakukan login. Berisikan field username dan Password
             <form action="<?=site_url('AccountController/login')?>" method="POST">
                 <h2 style="text-align: center;">Login</h2>
                 <label><b>Username</b></label>
                     <input type="text" name="username" placeholder="Username" class="input-field"/>
                 <label><b>Password</b></label>
                 <input type="password" name="password" placeholder="Password" class="input-field"/>
-                <div class="btn">	//Tombol untuk login
+                <div class="btn">
                     <button>Login</button>
                 </div>
             </form>
