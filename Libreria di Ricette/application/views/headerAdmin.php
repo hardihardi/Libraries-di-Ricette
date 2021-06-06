3b8b7ad19e95e9a23c16d09bccbe9cf6ffc58965
Mohammaddaffaharis <mohammaddaffaharis@gmail.com>
2021-06-06 11:29:18 +0700

    Create headerAdmin.php

---
+++ Libreria di Ricette/application/views/headerAdmin.php
@@ -0,0 +1,62 @@
+<html class="no-js" lang="eng">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Tasty Recipes</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/owl.carousel.min.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/magnific-popup.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/font-awesome.min.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/themify-icons.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/nice-select.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/flaticon.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/gijgo.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/animate.min.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/slick.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/slicknav.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/style.css') ?>">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets/css/home.css') ?>">
+</head>
+
+<body>
+    <!-- header-start -->
+    <!-- Header berisi Home, Recipes, dan About -->
+    <header>
+        <div class="header-area ">
+            <div id="sticky-header" class="main-header-area ">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-xl-3 col-lg-2">
+                            <div class="logo">
+                                <a href="index.html">
+                                    <img src="<?= base_url("assets/img/logo.png") ?>" alt="">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-lg-7">
+                            <div class="main-menu   d-none d-lg-block">
+                                <nav>
+                                    <ul id="navigation">
+                                        <li><a href="<?= base_url('viewMember')?>" style="color: white;">View User</a></li>
+                                        <li><a href="<?= base_url('addRefToko')?>" style="color: white;">view Bahan</a></li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="mobile_menu d-block d-lg-none"></div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- header-end -->
